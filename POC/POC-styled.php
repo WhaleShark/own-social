47cf2fa48454fbf63bb594aa96b0e747282ada55
Stacey Fenton <staceyfenton@gmail.com>
2013-07-26 16:27:57 +0100

    Alternative methods for Facebook and Google+ to get count of a specified URL

--- POC/POC-styled.php
+++ POC/POC-styled.php
@@ -41,6 +41,30 @@ function format_count($count)
 	}
 }
 
+function get_plusones($url) {
+ 
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, "https://clients6.google.com/rpc");
+    curl_setopt($curl, CURLOPT_POST, 1);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, '[{"method":"pos.plusones.get","id":"p","params":{"nolog":true,"id":"' . $url . '","source":"widget","userId":"@viewer","groupId":"@self"},"jsonrpc":"2.0","key":"p","apiVersion":"v1"}]');
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+    $curl_results = curl_exec ($curl);
+    curl_close ($curl);
+ 
+    $json = json_decode($curl_results, true);
+ 
+    return intval( $json[0]['result']['metadata']['globalCounts']['count'] );
+}
+
+function get_likes($url) {
+ 
+    $json_string = file_get_contents('http://graph.facebook.com/?ids=' . $url);
+    $json = json_decode($json_string, true);
+ 
+    return intval( $json[$url]['shares'] );
+}
+
 ?>
 
 <div class="sociable clearfix">
@@ -73,7 +97,7 @@ function format_count($count)
 	
 	<div class="btn-social-compact">
 		<a class="btn-gp" target="_blank" href="https://plus.google.com/share?url=<?php echo urlencode($Share_URL) ?>"><b>+1</b></a>
-		<div class="count count-gp"><i></i><u></u><?php echo format_count($google_data->plusOneCount) ?></div>
+		<div class="count count-gp"><i></i><u></u><? echo get_plusones('http://www.vouchercodes.co.uk/restaurant-vouchers.html'); ?></div>
 	</div>
 	
 	<div class="btn-follow">
